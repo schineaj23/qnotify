3bbd8dca56f5195730dc07807967e14e1815fccc
Andrew Schineller <schineaj23@pwcs-edu.org>
2020-06-10 16:38:58 -0400

    cleaned up a bit

--- python/main.py
+++ python/main.py
@@ -2,94 +2,81 @@
 import time, os
 import discord, asyncio
 
-token = "***REMOVED***"
-client = discord.Client()
+class Globals:
+    token = "***REMOVED***"
+    client = discord.Client()
+    reconnecting = False
+    start_time = time.time()
+    loop = asyncio.get_event_loop()
+    last_pos = 0
+
+    def get_state(self):
+        return self.reconnecting
+
+    def set_state(self, state):
+        self.reconnecting = state
+
+    def get_last_pos(self):
+        return self.last_pos
+    
+    def set_last_pos(self, new_pos):
+        self.last_pos = new_pos
+
+g = Globals()
 
 async def dm(msg):
-    user = await client.fetch_user(***REMOVED***)
+    user = await g.client.fetch_user(***REMOVED***)
     await user.send(embed=msg)
 
-reconnecting = False
-start_time = time.time()
-loop = asyncio.get_event_loop()
-
-def get_state():
-    global reconnecting
-    return reconnecting
-
-def set_state(state):
-    global reconnecting
-    reconnecting = state
+def notify(title, desc, color):
+    embed = discord.Embed()
+    embed.title = title
+    embed.description = desc
+    embed.color = color
+    g.loop.run_until_complete(dm(embed))
 
 def minute_passed():
-    global start_time
-    ret = time.time() - start_time >= 60
+    ret = time.time() - g.start_time >= 60
     if ret:
-        start_time = time.time()
+        g.start_time = time.time()
     return ret
 
 def hour_passed():
-    global start_time
-    ret = time.time() - start_time >= 3600
+    ret = time.time() - g.start_time >= 3600
     if ret:
-        start_time = time.time()
+        g.start_time = time.time()
     return ret
 
 def connected(cur_line, increased, first_time):
-    print(cur_line)
     index = cur_line.find("e:")
     current_pos = int(cur_line[3+index:]) # our current position in queue
-    print(current_pos)
-    print(get_state())
     loc_time = time.localtime()
+    if g.get_last_pos() == current_pos:
+            return
     if increased:
         print("Increased! sending msg")
-        embed = discord.Embed()
-        embed.title = "Welcome to queue!" if first_time else "Still in queue" 
-        embed.description = "Current Time: `{:0>2d}:{:0>2d}`\nCurrent Position: `{}`".format(loc_time.tm_hour, loc_time.tm_min, current_pos)
-        embed.color = 4437377
-        loop.run_until_complete(dm(embed))
-    elif get_state():
-        embed = discord.Embed()
-        embed.title = "Connected"
-        embed.description = "Connected to 2b2t Queue Server!\nCurrent Position: `{}`".format(current_pos)
-        embed.color = 4437377
-        loop.run_until_complete(dm(embed))
-        set_state(False)
+        notify("Welcome to queue!" if first_time else "Still in queue", "Current Time: `{:0>2d}:{:0>2d}`\nCurrent Position: `{}`".format(loc_time.tm_hour, loc_time.tm_min, current_pos), 4437377)
+    elif g.get_state():
+        notify("Connected", "Connected to 2b2t Queue Server!\nCurrent Position: `{}`".format(current_pos), 4437377)
+        g.set_state(False)
     elif current_pos == 1:
         print("about to join server!")
-        embed = discord.Embed()
-        embed.title = "First in Queue!"
-        embed.description = "Get to the computer now! You're first in line!"
-        embed.color = 7506394
-        loop.run_until_complete(dm(embed))
+        notify("First in Queue!", "Get to the computer now! You're first in line!", 7506394)
     elif current_pos <= 10:
         print("about to join server!")
-        embed = discord.Embed()
-        embed.title = "Close to joining!"
-        embed.description = "Heads up, you're in the top 10!\nCurrent Position: `{}`".format(current_pos)
-        embed.color = 16763904
-        loop.run_until_complete(dm(embed))
+        notify("Close to joining!", "Heads up, you're in the top 10!\nCurrent Position: `{}`".format(current_pos), 16763904)
+    g.set_last_pos(current_pos)
 
 
 def disconnected():
     loc_time = time.localtime()
     print("disconnected from 2b2t, sending msg")
-    print("\nDisconnected from 2b2t as of {:0>2d}:{:0>2d}! Attempting to reconnect.".format(loc_time.tm_hour, loc_time.tm_min))
-    embed = discord.Embed()
-    embed.title = "Disconnected"
-    embed.description = "Dropped from 2b2t Queue Server, attempting to reconnect.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(loc_time.tm_hour, loc_time.tm_min)
-    embed.color = 15746887
-    loop.create_task(dm(embed))
+    notify("Disconnected", "Dropped from 2b2t Queue Server, attempting to reconnect.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(loc_time.tm_hour, loc_time.tm_min), 15746887)
 
 def server_down():
     loc_time = time.localtime()
     print("server may be down, sending msg")
-    embed = discord.Embed()
-    embed.title = "Server Down"
-    embed.description = "You have been dropped from the server >5 times. 2b2t may be down.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(loc_time.tm_hour, loc_time.tm_min)
-    embed.color = 15746887
-    loop.create_task(dm(embed))
+    notify("Server Down", "You have been dropped from the server >5 times. 2b2t may be down.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(loc_time.tm_hour, loc_time.tm_min), 15746887)
 
 def follow(thefile):
     thefile.seek(0,2)
@@ -103,16 +90,16 @@ def follow(thefile):
 if __name__ == "__main__":
     logfile = open(os.getenv("APPDATA")+"/.minecraft/logs/latest.log", "r")
     loglines = follow(logfile)
-    set_state(False)
-    asyncio.set_event_loop(loop)
+    g.set_state(False)
+    asyncio.set_event_loop(g.loop)
     connection_count = 0
     try:
         print("logging into discord")
-        loop.create_task(client.login(token))
+        g.loop.create_task(g.client.login(g.token))
         print("connecting")
-        loop.create_task(client.connect())
+        g.loop.create_task(g.client.connect())
     except KeyboardInterrupt:
-        loop.run_until_complete(client.logout())
+        g.loop.run_until_complete(g.client.logout())
     print("connected, now running program")
     it = (line for line in loglines)
     for index, line in enumerate(it):
@@ -121,13 +108,15 @@ if __name__ == "__main__":
                 connected(line, True, True)
             connection_count = 0
             connected(line, hour_passed(), False)
-            print("{:0>2d}:{:0>2d}".format(time.localtime().tm_hour, time.localtime().tm_min))
         elif "Started saving saved containers in a new thread" in line:
             disconnected()
-            set_state(True)
+            g.set_state(True)
         elif "[main/INFO]: Connecting to 2b2t.org, 25565" in line:
             connection_count += 1
             print("can't connect", connection_count)
-            if connection_count >= 5 and not get_state():
+            if connection_count >= 5 and not g.get_state():
                 server_down()
-                set_state(True)
+                g.set_state(True)
+        elif "[CHAT] Connecting to the server..." in line:
+            notify("Joining 2b2t", "You made it! You are now joining 2b2t", 7506394)
+            exit()
