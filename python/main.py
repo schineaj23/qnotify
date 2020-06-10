b350e89fd598500e91bd64196e5ca57585140a10
Andrew Schineller <schineaj23@pwcs-edu.org>
2020-06-10 19:17:15 -0400

    actually fix this time

--- python/main.py
+++ python/main.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     try:
         logfile = open(os.getenv("APPDATA")+"/.minecraft/logs/latest.log", "r")
         loglines = follow(logfile)
-    except FileNotFoundError or TypeError:
+    except:
         logfile = open(".test/batch.txt")
         loglines = logfile.readlines()
         g.debug = True
