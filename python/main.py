af44fac848c511b44f6e764e956fd9edc01a9bc6
Andrew Schineller <34293975+schineaj23@users.noreply.github.com>
2020-06-10 20:11:46 -0400

    Tests working, Multi-platform support.  (#2)
    * Update python-app.yml
    
    * updated to do tests
    
    * update to only run main.py
    
    * update for debugging
    
    * updated to add STATS
    
    * updated tests and platform
    
    * typeerror for linux
    
    * actually fix this time
    
    * refactor
    
    * int()
    
    * save dm's for the actual messages, use server for the testing
    
    * delete temp files, ready for merge
    
    * added mac workflow and platform check

--- python/main.py
+++ python/main.py
@@ -1,6 +1,10 @@
 #! python3
-import time, os
-import discord, asyncio
+import os
+import sys
+import time
+import asyncio
+import discord
+
 
 class Globals:
     token = "NzIwMjc4MjA4OTIxOTkzMjk3.XuEDQQ.Zp76NSVPqY8OLrBvN1sd552yym8"
@@ -9,6 +13,9 @@ class Globals:
     start_time = time.time()
     loop = asyncio.get_event_loop()
     last_pos = 0
+    debug = False
+    start_pos = 0
+    first_time = True
 
     def get_state(self):
         return self.reconnecting
@@ -18,22 +25,40 @@ class Globals:
 
     def get_last_pos(self):
         return self.last_pos
-    
+
     def set_last_pos(self, new_pos):
         self.last_pos = new_pos
 
+
 g = Globals()
+
 
 async def dm(msg):
     user = await g.client.fetch_user(528600920787779607)
     await user.send(embed=msg)
 
-def notify(title, desc, color):
+
+async def txt(msg):
+    text_channel = await g.client.fetch_channel(720325502983864325)
+    await text_channel.send(embed=msg)
+
+
+def notify(title, desc, color, field=False, field_stats=""):
     embed = discord.Embed()
-    embed.title = title
-    embed.description = desc
-    embed.color = color
-    g.loop.run_until_complete(dm(embed))
+    if field:
+        embed.add_field(name="Your Stats", value=field_stats)
+    if g.debug:
+        embed.title = "DEBUG: " + title
+        embed.description = "**This is a test from GitHub**\n**Platform:** `{}`\n".format(
+            sys.platform) + desc
+        embed.color = color
+        g.loop.run_until_complete(txt(embed))
+    else:
+        embed.title = title
+        embed.description = desc
+        embed.color = color
+        g.loop.run_until_complete(dm(embed))
+
 
 def minute_passed():
     ret = time.time() - g.start_time >= 60
@@ -41,44 +66,59 @@ def minute_passed():
         g.start_time = time.time()
     return ret
 
+
 def hour_passed():
     ret = time.time() - g.start_time >= 3600
     if ret:
         g.start_time = time.time()
     return ret
 
-def connected(cur_line, increased, first_time):
+
+def connected(cur_line, increased):
     index = cur_line.find("e:")
-    current_pos = int(cur_line[3+index:]) # our current position in queue
+    current_pos = int(cur_line[3+index:])  # our current position in queue
     loc_time = time.localtime()
     if g.get_last_pos() == current_pos:
-            return
+        return
+    if g.first_time:
+        g.start_pos = current_pos
     if increased:
         print("Increased! sending msg")
-        notify("Welcome to queue!" if first_time else "Still in queue", "Current Time: `{:0>2d}:{:0>2d}`\nCurrent Position: `{}`".format(loc_time.tm_hour, loc_time.tm_min, current_pos), 4437377)
+        msg = "Current Time: `{:0>2d}:{:0>2d}`\nCurrent Position: `{}`".format(
+            loc_time.tm_hour, loc_time.tm_min, current_pos)
+        notify("Welcome to queue!" if g.first_time else "Still in queue", msg, 4437377)
     elif g.get_state():
-        notify("Connected", "Connected to 2b2t Queue Server!\nCurrent Position: `{}`".format(current_pos), 4437377)
+        notify("Connected", "Connected to 2b2t Queue Server!\nCurrent Position: `{}`".format(
+            current_pos), 4437377)
         g.set_state(False)
     elif current_pos == 1:
         print("about to join server!")
-        notify("First in Queue!", "Get to the computer now! You're first in line!", 7506394)
+        notify("First in Queue!",
+               "Get to the computer now! You're first in line!", 7506394)
     elif current_pos <= 10:
         print("about to join server!")
-        notify("Close to joining!", "Heads up, you're in the top 10!\nCurrent Position: `{}`".format(current_pos), 16763904)
+        notify("Close to joining!", "Heads up, you're in the top 10!\nCurrent Position: `{}`".format(
+            current_pos), 16763904)
+    g.first_time = False
     g.set_last_pos(current_pos)
+
 
 def disconnected():
     loc_time = time.localtime()
     print("disconnected from 2b2t, sending msg")
-    notify("Disconnected", "Dropped from 2b2t Queue Server, attempting to reconnect.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(loc_time.tm_hour, loc_time.tm_min), 15746887)
+    notify("Disconnected", "Dropped from 2b2t Queue Server, attempting to reconnect.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(
+        loc_time.tm_hour, loc_time.tm_min), 15746887)
+
 
 def server_down():
     loc_time = time.localtime()
     print("server may be down, sending msg")
-    notify("Server Down", "You have been dropped from the server >5 times. 2b2t may be down.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(loc_time.tm_hour, loc_time.tm_min), 15746887)
+    notify("Server Down", "You have been dropped from the server >5 times. 2b2t may be down.\nCurrent Time: `{:0>2d}:{:0>2d}`".format(
+        loc_time.tm_hour, loc_time.tm_min), 15746887)
+
 
 def follow(thefile):
-    thefile.seek(0,2)
+    thefile.seek(0, 2)
     while True:
         line = thefile.readline()
         if not line:
@@ -86,9 +126,21 @@ def follow(thefile):
             continue
         yield line
 
+
 if __name__ == "__main__":
-    logfile = open(os.getenv("APPDATA")+"/.minecraft/logs/latest.log", "r")
-    loglines = follow(logfile)
+    try:
+        if sys.platform.startswith('win32'):
+            logfile = open(os.getenv("APPDATA") +
+                           "/.minecraft/logs/latest.log", "r")
+        elif sys.platform.startswith('linux'):  # linux people be like:
+            logfile = open("~/.minecraft/logs/latest.log", "r")
+        else:  # mac people be like
+            logfile = open("~/Library/Application Support/minecraft", "r")
+        loglines = follow(logfile)
+    except:  # imagine not having minecraft
+        logfile = open(".test/batch.txt")
+        loglines = logfile.readlines()
+        g.debug = True
     g.set_state(False)
     asyncio.set_event_loop(g.loop)
     try:
@@ -98,24 +150,29 @@ if __name__ == "__main__":
         g.loop.create_task(g.client.connect())
     except KeyboardInterrupt:
         g.loop.run_until_complete(g.client.logout())
-    connection_count = 0
+    CONNECTION_COUNT = 0
+    TOTAL_CONNECTION_COUNT = 0
+    start_time = time.time()
     print("connected, now running program")
     it = (line for line in loglines)
     for index, line in enumerate(it):
         if "[main/INFO]: [CHAT] Position in queue:" in line:
-            if index == 0: # print our first position
-                connected(line, True, True)
-            connection_count = 0
-            connected(line, hour_passed(), False)
+            if g.first_time:  # print our first position
+                connected(line, True)
+            CONNECTION_COUNT = 0
+            connected(line, hour_passed())
         elif "Started saving saved containers in a new thread" in line:
             disconnected()
             g.set_state(True)
         elif "[main/INFO]: Connecting to 2b2t.org, 25565" in line:
-            connection_count += 1
-            print("can't connect", connection_count)
-            if connection_count >= 5 and not g.get_state():
+            CONNECTION_COUNT += 1
+            TOTAL_CONNECTION_COUNT += 1
+            print("can't connect", CONNECTION_COUNT)
+            if CONNECTION_COUNT >= 5:
                 server_down()
                 g.set_state(True)
+                CONNECTION_COUNT = 0
         elif "[CHAT] Connecting to the server..." in line:
-            notify("Joining 2b2t", "You made it! You are now joining 2b2t", 7506394)
-            exit()
+            notify("Joining 2b2t", "You made it! You are now joining 2b2t", 7506394, True, "Time Elapsed: `{}` minutes\nStarting Position: `{}`\nDisconnections: `{}`".format(
+                int((time.time()-g.start_time)/60), g.start_pos, TOTAL_CONNECTION_COUNT))
+            sys.exit()
